8bbc0e0483137fd13d679dbf0faf99de83ce2afc
JacobLinCool <jacoblincool@gmail.com>
2021-06-19 01:14:00 +0800

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+function gen_data(document) {
+    let list = [...document.querySelectorAll("a.theme-list-main")];
+    let data = list.map((x) => {
+        return {
+            name: x.querySelector(".theme-name").innerHTML,
+            type: document.querySelector(".theme-title").innerHTML.trim(),
+            ep: number_normalize(x.querySelector(".theme-number").innerHTML),
+            view: number_normalize(x.querySelector(".show-view-number > p").innerHTML),
+            image: x.querySelector(".theme-img").src,
+            time: [...x.querySelector(".theme-time").innerHTML.matchAll(/\d{4}\/\d{2}/g)][0][0],
+        };
+    });
+
+    for (let i = 0; i < data.length; i++) {
+        data[i].view_avg = +(data[i].view / data[i].ep).toFixed(2);
+        data[i].year = parseInt(data[i].time.split("/")[0]);
+        data[i].month = parseInt(data[i].time.split("/")[1]);
+        data[i].time = undefined;
+    }
+
+    return data;
+}
+
+function number_normalize(n) {
+    try {
+        let number = parseFloat([...n.matchAll(/[+-]?([0-9]*[.])?[0-9]+/g)][0][0]);
+        if (n.includes("萬")) return number * 10000;
+        else return number;
+    } catch (e) {}
+    return 0;
+}
+
+export { gen_data };
---
+++ src/main.js
@@ -0,0 +1,60 @@
+import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
+import fs from "fs";
+import { gen_data } from "./data.js";
+
+async function main() {
+    if (!fs.existsSync("./dist/")) fs.mkdirSync("./dist/");
+
+    await full_time_analytics();
+    await monthly_analytics();
+}
+
+async function full_time_analytics() {
+    const dist = "./dist/full/";
+    if (!fs.existsSync(dist)) {
+        fs.mkdirSync(dist);
+    }
+    for (let c = 1; c <= 13; c++) {
+        let type = "",
+            collection = [];
+        let p = 0;
+        while (++p) {
+            console.log(`c = ${c}, p = ${p}`);
+            let raw = await fetch(`https://ani.gamer.com.tw/animeList.php?page=${p}&c=${c}&sort=1`).then((r) => r.text());
+            let dom = new JSDOM(raw);
+            type = dom.window.document.querySelector(".theme-title").innerHTML.trim();
+            let data = gen_data(dom.window.document);
+            collection = collection.concat(data);
+            if (!data.length) break;
+        }
+        collection.sort((a, b) => b.view_avg - a.view_avg);
+        console.log(collection);
+        fs.writeFileSync(`${dist}${type}.json`, JSON.stringify(collection, null, 2));
+    }
+}
+async function monthly_analytics() {
+    const dist = "./dist/monthly/";
+    if (!fs.existsSync(dist)) {
+        fs.mkdirSync(dist);
+    }
+    for (let c = 1; c <= 13; c++) {
+        let type = "",
+            collection = [];
+        let p = 0;
+        while (++p) {
+            console.log(`c = ${c}, p = ${p}`);
+            let raw = await fetch(`https://ani.gamer.com.tw/animeList.php?page=${p}&c=${c}&sort=2`).then((r) => r.text());
+            let dom = new JSDOM(raw);
+            type = dom.window.document.querySelector(".theme-title").innerHTML.trim();
+            let data = gen_data(dom.window.document);
+            collection = collection.concat(data);
+            if (!data.length) break;
+        }
+        collection.sort((a, b) => b.view_avg - a.view_avg);
+        console.log(collection);
+        fs.writeFileSync(`${dist}${type}.json`, JSON.stringify(collection, null, 2));
+    }
+}
+
+export { main };
